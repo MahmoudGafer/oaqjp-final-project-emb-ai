2fa0f5d4d54e442b11d1c2c9e6726631ce704594
MahmoudGafer <gafermahmoud677@gmail.com>
2025-08-28 17:25:56 -0400

    done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,9 +4,16 @@ import json
 def emotion_detector(text_to_analyze):
     URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    if not text_to_analyze or text_to_analyze.strip() == "":
+        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None,}
+
     Input_json = {"raw_document": {"text": text_to_analyze}}
-    
     response = requests.post(URL, json=Input_json, headers=Headers)
+
+    if response.status_code == 400:
+        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None,}
+
     formatted_text = json.loads(response.text)
     formatted_response = formatted_text['emotionPredictions'][0]
 
@@ -15,13 +22,10 @@ def emotion_detector(text_to_analyze):
     fear_score = formatted_response['emotion']['fear']
     joy_score = formatted_response['emotion']['joy']
     sadness_score = formatted_response['emotion']['sadness']
-
-    emotions = {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score,
-                'joy': joy_score, 'sadness': sadness_score,}
-
+    emotions = {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score,}
     dominant_emotion = max(emotions, key=emotions.get)      
     
-    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score,
-            'joy': joy_score,'sadness': sadness_score, 'dominant_emotion': dominant_emotion}
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score,'sadness': sadness_score, 
+            'dominant_emotion': dominant_emotion}
 
 
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+''' Emotion Detection APP '''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,8 +6,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect_emotions():
+    ''' analyze input texts '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
 
     anger = response['anger']
     disgust = response['disgust']
@@ -15,13 +20,12 @@ def detect_emotions():
     sadness = response['sadness']
     dom = response['dominant_emotion']
 
-    return (f"For the given statement, the system response is "
-        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
-        f"'joy': {joy}, 'sadness': {sadness}. "
-        f"The dominant emotion is {dom}.")
+    return (f"For the given statement, the system response is " f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+            f"'joy': {joy}, 'sadness': {sadness}. "f"The dominant emotion is {dom}.")
 
 @app.route("/")
 def render_index_page():
+    ''' render the html page '''
     return render_template('index.html')
 
 if __name__ == "__main__":
